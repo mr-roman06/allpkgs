e4bd92a34a7c93c9211b8fa5472e251548d2a9b8
MR.ROMAN <106998086+mr-roman06@users.noreply.github.com>
2022-06-15 14:45:14 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,26 @@
+apt-get update -y
+apt-get upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pip install lolcat
+pkg install gedit -y
+pkg install nano -y
+pkg install fish -y
+pkg install ruby -y
+pkg install php -y
+pkg install perl -y
+pkg install bash -y
+pkg install perl -y
+pkg install cowsay -y
+pkg install zip -y
+pkg install unzip -y
+pkg install cmatrix -y
+pkg install openssh -y
+pkg install vim -y
+pkg install zsh -y
+pkg install tch -y
+pkg install fortune -y
+pkg install figlet -y
+apt update && apt upgrade -y
+
